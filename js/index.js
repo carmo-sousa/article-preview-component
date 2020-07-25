4fe3f06f265503af28ba9524e67cd962bfebe723
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-07-25 15:40:40 -0400

    Integrenado o script
    Integrando com o script que adiciona a classe `.active` no `.card-social` e no `.share`

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const share = document.querySelector('.share');
+const social = document.querySelector('.card-social');
+
+share.addEventListener('click', () => {
+  let className = social.className.split(' ')
+  let active = className.length > 1 ? true : false;
+
+  if (active) {
+    social.classList.remove('active');
+    share.classList.remove('active');
+
+  } else {
+    social.classList.add('active');
+    share.classList.add('active');
+
+  }
+});
