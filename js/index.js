8ad91baf8aeb9d185a5b5bd54950acadccc5e2e3
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-08-01 23:03:06 -0400

    Refatorando o js/index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-const share = document.querySelector('.share');
-const social = document.querySelector('.card-social');
+const share = document.querySelector('.icon-share');
+// const social = document.querySelector('.card-social');
 
 share.addEventListener('click', () => {
   let className = social.className.split(' ')
   let active = className.length > 1 ? true : false;
 
   if (active) {
-    social.classList.remove('active');
+    // social.classList.remove('active');
     share.classList.remove('active');
 
   } else {
-    social.classList.add('active');
+    // social.classList.add('active');
     share.classList.add('active');
 
   }
